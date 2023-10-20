11f991503a3893a975ee86bec311eba9e7982b7d
rrhinox <rrhino25@yahoo.com>
2023-10-21 00:11:49 +0200

    Fix IPs into hosts file

--- vagrant-cluster/scripts/common.sh
+++ vagrant-cluster/scripts/common.sh
@@ -7,6 +7,6 @@ sudo ufw disable
 sudo swapoff -a
 
 # add into /etc/hosts
-sudo echo "192.168.0.0 cplane.kube" >> /etc/hosts
-sudo echo "192.168.0.1 worker01.kube" >> /etc/hosts
-sudo echo "192.168.0.2 worker02.kube" >> /etc/hosts
+sudo echo "192.168.0.10 cplane.kube" >> /etc/hosts
+sudo echo "192.168.0.11 worker01.kube" >> /etc/hosts
+sudo echo "192.168.0.12 worker02.kube" >> /etc/hosts
