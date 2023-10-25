5ad275281d690558873fbc3417323645f4ba09c3
rrhinox <rrhino25@yahoo.com>
2023-10-25 02:36:02 +0200

    Add some useful guides and command to run manually

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ The idea is to use the kubeadm tools offered by the official documentation :
 + <https://kubernetes.io/docs/tasks/tools/>
 + <https://kubernetes.io/docs/reference/setup-tools/kubeadm/>
 + <https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/>
++ <https://kubernetes.io/docs/home/>
 
 ## 🧪 Lab Prerequisites
 The lab requires the installation and use of the following tools
@@ -33,3 +34,16 @@ The Vagrantfile has been tested for now only with :
 + Virtual box 7.0.10 r158379 (Qt5.15.2)
 + Vagrant 2.4.0
 
+> 🐛 (i) Known issue with gitbash for windows and Vagrant : 
++ if you use git-bash.exe the Ctrl + C exit from ssh session opened via `vagrant ssh` command
++ Vagrant create 2 network interface (eth0 and eth1) this needs to be taken into account at cluster init via kubeadm configuration file, see vagrant-cluster/scripts/common.sh:line:76 (podSubnet).
+
+## 🦮 Guides that I highly recommend to follow!
+
+This guides help me a lot to understand all the aspects of the CKA Exam and also inspire me to create a Vagrantfile to follow step by step all the labs manually of course offered by The Linux Foundation [LFS258](https://training.linuxfoundation.org/training/kubernetes-fundamentals/)
+
++ [DevOpsCube - Exam Guide](https://devopscube.com/cka-exam-study-guide/)
++ [DevOpsCube - Automated Vagrant Cluster](https://devopscube.com/kubernetes-cluster-vagrant/)
++ [Vagrant Troubleshoot](https://jhooq.com/kubernetes-error-execution-phase-preflight-preflight/)
++ [Troubleshooting kubeadm all topics](https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/troubleshooting-kubeadm/)
++ [Vagrant topic for troubleshooting kubeadm](https://jhooq.com/kubernetes-error-execution-phase-preflight-preflight/)
--- vagrant-cluster/scripts/common.sh
+++ vagrant-cluster/scripts/common.sh
@@ -29,3 +29,51 @@ EOF
 
 # Ensure the changes
 sudo sysctl --system
+
+# ----- Install keys and repo for containerd ----- #
+# sudo mkdir -p /etc/apt/keyrings
+# ----- #
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+# | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# sudo apt-get update && sudo apt-get install containerd.io -y
+# ----- #
+# echo \
+# "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+# https://download.docker.com/linux/ubuntu \
+# $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# ----- Containerd ----- #
+# sudo apt-get update && sudo apt-get install containerd.io -y
+# sudo containerd config default | sudo tee /etc/containerd/config.toml
+# sudo sed -e 's/SystemdCgroup = false/SystemdCgroup = true/g' -i /etc/containerd/config.toml
+# sudo systemctl restart containerd
+
+# ----- Install Kubernetes SW ----- #
+# echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+# sudo apt-get update
+# sudo apt-get install -y kubeadm=1.25.1-00 kubelet=1.25.1-00 kubectl=1.25.1-00
+
+# ----- Hold the software at the recent but stable version we install ----- #
+# sudo apt-mark hold kubeadm kubelet kubectl
+
+# ----- Using Calico as a Network plugin will allow to use Network Policy later ----- #
+# wget https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+# grep -A4 -B3 CALICO_IPV4POOL_CIDR calico.yaml
+    # The default IPv4 pool to create on startup if none exists. Pod IPs will be
+    # chosen from this range. Changing this value after installation will have
+    # no effect. This should fall within `--cluster-cidr`.
+    # - name: CALICO_IPV4POOL_CIDR
+    #   value: "192.168.0.0/16"
+    # Disable file logging so `kubectl logs` works.
+
+# ----- kubeadm-config.yaml ----- #
+# apiVersion: kubeadm.k8s.io/v1beta3
+# kind: ClusterConfiguration
+# kubernetesVersion: 1.25.1 #<-- Use the word stable for newest version
+# controlPlaneEndpoint: "k8scp:6443" #<-- Use the alias we put in /etc/hosts not the IP
+# networking:
+#   podSubnet: 192.168.0.0/16 #<-- Match the IP range from the Calico config file
+
+# sudo kubeadm init --config kubeadm-config.yaml --upload-certs | tee /tmp/kubeadm-init.out
+
